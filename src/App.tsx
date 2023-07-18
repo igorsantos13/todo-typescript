ff158fcbb9ee4e828d78c92d543074a3abf4b34a
Igor Santos <igorsantos9513@outlook.com>
2023-07-18 17:51:05 -0300

    fix: input and add task

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,50 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import './App.css'
 
-interface TodoItem {
-  id: number,
-  value: string
-}
-
-let count:number = 1
-
 function App() {
-  const [list, setList] = useState<TodoItem[]>([{id: 0, value: ''}])
+  const [task, setTask] = useState<string>('')
+  const [list, setList] = useState<string[]>([])
 
-  const handleChange = (value: string, id: TodoItem['id']) => {
-    setList(prev => prev.map(item => item.id === id ? {...item, value}: item))
+  const handleClick = (e:FormEvent) => {
+    e.preventDefault()
+
+    if(task === ''){
+      alert('type a task before adding')
+      return ''
+    }
+
+    setList((prevTasks)=>[...prevTasks, task])
+
+    setTask('')
   }
 
-  const handleDelete = (id: TodoItem['id']) => {
-    setList(prev => prev.filter(item => item.id !== id))
-  }
+  // const handleDelete = (id: TodoItem['id']) => {
+  //   setList(prev => prev.filter(item => item.id !== id))
+  // }
 
-  const handleAdd = (index: number) => {
-    const newItem = { id: count++, value: '' }
-    setList(prev => [...prev.slice(0, index + 1), newItem, ...prev.slice(index + 1)])
-  }
+  // const handleAdd = (index: number) => {
+  //   const newItem = { id: count++, value: '' }
+  //   setList(prev => [...prev.slice(0, index + 1), newItem, ...prev.slice(index + 1)])
+  // }
 
   return (
     <div>
-      {list.map((item, index) => (
-        <div key={item.id}>
-          <input type="text" value={item.value} onChange={e => handleChange(e.currentTarget.value, item.id)}/>
+      <form action="">
+      <input type="text" placeholder='type your task' onChange={(event)=> setTask(event?.target.value)} value={task}/> 
+      <button type='submit' onClick={(event)=> handleClick(event)}>Add task</button>
 
-          <button onClick={() => handleAdd(index)}>add task</button>
-          {list.length > 1 && (
-            <button onClick={()=>handleDelete(item.id)}>remove task</button>
-          )}
-        </div>
-      ))}
+      {/* {task > 1 (
+        <button>remove task</button>
+      )} */}
+      </form>
+
+      <div>
+        <ul>
+          {list.map(item => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
